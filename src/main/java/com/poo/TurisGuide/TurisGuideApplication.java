4bb06a2376fc8e3346a0e02347d38305e98a213f
Guilherme <guilhermeemeterioif@gmail.com>
2025-10-30 20:30:50 -0300

    first commit

---
+++ src/main/java/com/poo/TurisGuide/TurisGuideApplication.java
@@ -0,0 +1,13 @@
+package com.poo.TurisGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurisGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurisGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poo/TurisGuide/TurisGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.TurisGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurisGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
